a7bd21b0cdbf3c4237f375b1a37a4aad0ceac61b
devsocialadforge <devsocialadforge@gmail.com>
2026-02-07 18:46:00 +0400

    added svg and logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,29 +6,27 @@ import sitemap from '@astrojs/sitemap';
 
 import vercel from '@astrojs/vercel';
 
+import favicons from 'astro-favicons';
+
 export default defineConfig({
   site: 'https://atozbusiness.ae',
-  integrations: [
-    react(),
-    tailwind(),
-    sitemap({
-      filter: (page) =>
-        !page.includes('/studio') &&
-        !page.includes('/admin') &&
-        !page.includes('/components-demo'),
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      customPages: [
-        'https://atozbusiness.ae/',
-        'https://atozbusiness.ae/about',
-        'https://atozbusiness.ae/contact',
-        'https://atozbusiness.ae/careers',
-        'https://atozbusiness.ae/privacy-policy',
-        'https://atozbusiness.ae/terms-of-service',
-      ],
-    }),
-  ],
+  integrations: [react(), tailwind(), sitemap({
+    filter: (page) =>
+      !page.includes('/studio') &&
+      !page.includes('/admin') &&
+      !page.includes('/components-demo'),
+    changefreq: 'weekly',
+    priority: 0.7,
+    lastmod: new Date(),
+    customPages: [
+      'https://atozbusiness.ae/',
+      'https://atozbusiness.ae/about',
+      'https://atozbusiness.ae/contact',
+      'https://atozbusiness.ae/careers',
+      'https://atozbusiness.ae/privacy-policy',
+      'https://atozbusiness.ae/terms-of-service',
+    ],
+  }), favicons()],
   output: 'static',
   adapter: vercel({
     imagesConfig: {
