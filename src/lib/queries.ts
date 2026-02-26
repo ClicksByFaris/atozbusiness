66f8c581bfbe3e9aeefc7412ab467bf0c165c1de
devsocialadforge <devsocialadforge@gmail.com>
2026-02-26 15:12:37 +0400

    add lp home

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,5 +1,24 @@
 // GROQ queries for Sanity CMS
 
+export const latestLandingPagesQuery = `*[_type == "landingPage" && published == true && active == true] | order(_createdAt desc)[0...3] {
+  title,
+  slug,
+  mainHeadline,
+  subHeadline,
+  heroImage {
+    asset->{
+      _id,
+      url,
+      metadata {
+        lqip,
+        dimensions
+      }
+    },
+    altText
+  },
+  primaryCtaText
+}`;
+
 // About
 export const aboutPageQuery = `*[_type == "about"][0] {
   teamMembers[] | order(order asc) {
