379a1c3214b9db19e9705be2214d1af9447d192a
Clicks by Faris <310061761+ClicksByFaris@users.noreply.github.com>
2026-08-29 18:27:49 +0400

    add new version gtm mangager

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 import vercel from '@astrojs/vercel';
 
@@ -11,14 +12,24 @@ import favicons from 'astro-favicons';
 export default defineConfig({
   site: 'https://atozbusiness.ae',
   trailingSlash: 'ignore',
-  integrations: [react(), tailwind(), sitemap({
-    filter: (page) =>
-      !page.includes('/studio') &&
-      !page.includes('/admin'),
-    changefreq: 'weekly',
-    priority: 0.7,
-    lastmod: new Date(),
-  }), favicons()],
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap({
+      filter: (page) =>
+        !page.includes('/studio') &&
+        !page.includes('/admin'),
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+    favicons(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   output: 'server',
   adapter: vercel({
     imagesConfig: {
